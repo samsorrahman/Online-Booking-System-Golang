30c78561452ff6d4b27ac864b6bb8c3f404ab5c1
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-09-05 11:30:16 -0700

    Project setup

---
+++ cmd/web/routes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/samsorrahman/Online-Booking-System-Golang/pkg/config"
+	"github.com/samsorrahman/Online-Booking-System-Golang/pkg/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	// mux := pat.New()
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// return mux
+
+	// Using chi router
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	return mux
+
+}
