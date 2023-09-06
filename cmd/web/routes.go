5620ae09367f346c6bab3f6f1689aeacb54fca3e
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-09-06 06:10:30 -0700

    Converting html into golang tmpls

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,13 +10,6 @@ import (
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	// mux := pat.New()
-	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-	// return mux
-
-	// Using chi router
-
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
@@ -25,9 +18,15 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
+
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
-	return mux
 
+	return mux
 }
