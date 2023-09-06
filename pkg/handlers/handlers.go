02786d7b2e1d4047f456c14f1741d47887d49f8c
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-09-06 07:31:51 -0700

    Creating a handler that return a JSON

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,9 +1,10 @@
 package handlers
 
 import (
-	"net/http"
-
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/samsorrahman/Online-Booking-System-Golang/pkg/config"
 	"github.com/samsorrahman/Online-Booking-System-Golang/pkg/models"
@@ -81,6 +82,29 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	end := r.Form.Get("end")
 
 	w.Write([]byte(fmt.Sprintf("start date is %s and end is %s", start, end)))
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// AvailabilityJSON handles request for availability and sends JSON response
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK:      true,
+		Message: "Available!",
+	}
+
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println(string(out))
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 // Contact renders the contact page
